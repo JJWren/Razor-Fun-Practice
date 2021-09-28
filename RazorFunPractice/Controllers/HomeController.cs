003b1d9570e3a334eac8e32592d8ea8c4eedab88
Joshua Wren <43586816+JJWren@users.noreply.github.com>
2021-09-28 17:09:18 -0500

    Added HomeController and View with Index

---
+++ RazorFunPractice/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorFunPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("index")]
+        public ViewResult Index()
+        {
+            // This will serve //Views/Home/Index.cshtml
+            return View();
+        }
+    }
+}
